f81b2ba769200e21ee68be2ad39a48fc2dab1c15
Leo Karachevtsev <LKarachevtsev@ssc.beeline.ru>
2024-03-28 14:39:22 +0300

    fix alerts

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from pytest import mark, param
 
+from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 
@@ -19,18 +20,20 @@ LINKS = [
 ]
 
 @mark.parametrize('link', LINKS)
+@mark.skip
 def test_guest_can_add_product_to_basket(browser: WebDriver, link: str) -> None:
     page = ProductPage(browser, link)
     page.open()
 
-    page.should_success_add_product_to_cart()
+    page.should_success_add_product_to_basket()
 
 
+@mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser: WebDriver) -> None:
     page = ProductPage(browser, LINK)
     page.open()
 
-    page.should_success_banner_not_exist_after_add_to_cart()
+    page.should_success_banner_not_exist_after_add_to_basket()
 
 
 def test_guest_cant_see_success_message(browser: WebDriver) -> None:
@@ -40,11 +43,12 @@ def test_guest_cant_see_success_message(browser: WebDriver) -> None:
     page.should_success_banner_not_exist()
 
 
+@mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser: WebDriver) -> None:
     page = ProductPage(browser, LINK)
     page.open()
 
-    page.should_success_banner_not_exist_after_after_add_product_to_cart()
+    page.should_success_banner_not_exist_after_after_add_product_to_basket()
 
 
 def test_guest_should_see_login_link_on_product_page(browser: WebDriver) -> None:
@@ -59,3 +63,19 @@ def test_guest_can_go_to_login_page_from_product_page(browser: WebDriver) -> Non
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser: WebDriver) -> None:
+    page = ProductPage(browser, LINK)
+    page.open()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser: WebDriver) -> None:
+    page = ProductPage(browser, LINK)
+    page.open()
+
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_basket_page()
+
+    basket_page.should_be_backet_empty()
