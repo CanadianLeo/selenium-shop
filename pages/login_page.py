dc5c148740738abf21f3fd0a398eb98f1edd3f31
Leo Karachevtsev <LKarachevtsev@ssc.beeline.ru>
2024-03-28 12:15:56 +0300

    add login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,13 +10,16 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
+
     def should_be_login_url(self: Self):
         # реализуйте проверку на корректный url адрес
         assert "login" in self.browser.current_url
 
+
     def should_be_login_form(self: Self):
         # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM)
+
 
     def should_be_register_form(self: Self):
         # реализуйте проверку, что есть форма регистрации на странице
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 LINK = "http://selenium1py.pythonanywhere.com/"
@@ -9,6 +10,8 @@ def test_guest_can_go_to_login_page(browser: WebDriver):
     page = MainPage(browser, LINK)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser: WebDriver):
