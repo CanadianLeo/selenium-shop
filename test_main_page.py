003597e56f66146f8476e03802873c1f03cdb844
Leo Karachevtsev <LKarachevtsev@ssc.beeline.ru>
2024-03-28 15:25:23 +0300

    agregate login test in class

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,6 @@
+from typing_extensions import Self
+
+from pytest import mark
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 from pages.basket_page import BasketPage
@@ -6,19 +9,20 @@ from pages.main_page import MainPage
 
 LINK = "http://selenium1py.pythonanywhere.com/"
 
+@mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self: Self, browser: WebDriver) -> None:
+        page = MainPage(browser, LINK)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_can_go_to_login_page(browser: WebDriver) -> None:
-    page = MainPage(browser, LINK)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
 
-
-def test_guest_should_see_login_link(browser: WebDriver) -> None:
-    page = MainPage(browser, LINK)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self: Self, browser: WebDriver) -> None:
+        page = MainPage(browser, LINK)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser: WebDriver) -> None:
