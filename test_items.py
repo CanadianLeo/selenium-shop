cac6693bae3ef5a3f511ac6e5967487ecd431aa4
Leo Karachevtsev <LKarachevtsev@ssc.beeline.ru>
2024-03-28 10:01:47 +0300

    refactor test name

--- test_items.py
+++ test_items.py
@@ -2,8 +2,7 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
 
-def test_add_to_cart_button(browser: WebDriver):
+def test_add_to_cart_button_exist(browser: WebDriver):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
 
-    button = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
-    assert button, "Кнопка 'Добавить в корзину' не найдена"
+    assert browser.find_elements(By.CLASS_NAME, "btn-add-to-basket"), "Кнопка 'Добавить в корзину' не найдена"
