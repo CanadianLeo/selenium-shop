097f5c9a11b8e2345e2abdcdebd26f99b05f4912
Leo Karachevtsev <LKarachevtsev@ssc.beeline.ru>
2024-03-28 11:37:22 +0300

    add is_element_present for waiting element on page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,19 @@
 from typing_extensions import Self
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage:
-    def __init__(self: Self, browser: WebDriver, url: str):
+    def __init__(self: Self, browser: WebDriver, url: str, timeout: int = 10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self: Self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -10,3 +10,9 @@ def test_guest_can_go_to_login_page(browser: WebDriver):
     page = MainPage(browser, LINK)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, LINK)
+    page.open()
+    page.should_be_login_link()
