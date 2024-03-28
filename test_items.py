7ba4e48a35dd30eb748940a8299302e83ccedb7e
Leo Karachevtsev <LKarachevtsev@ssc.beeline.ru>
2024-03-28 11:11:19 +0300

    add test_guest_can_go_to_login_page

--- test_items.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-
-
-def test_add_to_cart_button_exist(browser: WebDriver):
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-
-    assert browser.find_elements(By.CLASS_NAME, "btn-add-to-basket"), "Кнопка 'Добавить в корзину' не найдена"
